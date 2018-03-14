0af763c0938c6748d136cb50a7e69d214f47a5f6
Berkowitz <adb16130@d71h126.pr.uconn.edu>
2018-03-14 14:55:10 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a work in progress. It will (hopefully) be the repo for an npm module.
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: ["./src/js/index.js"],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: ['.js', '.scss']
+}
