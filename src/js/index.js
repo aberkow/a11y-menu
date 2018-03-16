9b691f43e9b1f3e4d74578923c67e223f8a71da6
Berkowitz <adb16130@d71h126.pr.uconn.edu>
2018-03-16 13:37:17 -0400

    needs to be better....

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,6 @@
 import { displayMenu } from './utils/displayMenu';
 
-// const data = require('../mock-data/mock-menu.json');
 const testData = require('../mock-data/test-data.json');
 const menu = document.getElementById('main-menu');
-
-// displayMenu(menu, data.menu);
 
 displayMenu(menu, testData.menu);
