d3eaabe5a3ad748a0f7e8dbf657bf224abd91a45
Berkowitz <adb16130@d71h126.pr.uconn.edu>
2018-03-16 14:26:45 -0400

    added support for generating styles via webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: ["./src/js/index.js"],
+  entry: ["./src/js/index.js", "./src/scss/style.scss"],
   output: {
     path: path.resolve(__dirname, 'public/build'),
     filename: 'bundle.js'
@@ -19,9 +20,19 @@ module.exports = {
           }
         }
       },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
     ]
   },
   resolve: {
     extensions: ['.js', '.json', '.scss']
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
 }
