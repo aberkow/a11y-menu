2f1185b4933e3e911a54a267eebcebf8bbed9ce6
Adam Berkowitz <berkowitz.clarinet@gmail.com>
2018-06-07 11:51:14 -0400

    set defaults for the constructor and updated readme

--- src/js/index.js
+++ src/js/index.js
@@ -4,26 +4,10 @@ import Navigation from './Navigation/Navigation';
 const testData = require('../mock-data/test-data.json');
 const mainMenu = document.getElementById('main-menu');
 
-// an example with basic characters.
-// const menuOpts = {
-//   fontAwesome: false,
-//   chevronDown: '+',
-//   chevronUp: '-'
-// }
-
-// the fontawesome icons need to be in the escaped html format. 
-const menuOpts = {
-  // the current font-family choices
-  // ['FontAwesome', 'Font Awesome 5 Free', 'Glyphicons Halflings']
-  fontFamily: 'Font Awesome 5 Free',
-  chevronDown: '\\f078',
-  chevronUp: '\\f077'
-}
-
-const navigation = new Navigation(menuOpts);
+const navigation = new Navigation();
 
 displayMenu(mainMenu, testData.menu);
 
 document.addEventListener('DOMContentLoaded', () => {
-  navigation.init(mainMenu);
+  navigation.init();
 });
