40d02c9ba0ae186c811e4eab447d72884c21f65c
Adam Berkowitz <berkowitz.clarinet@gmail.com>
2018-06-07 11:01:47 -0400

    working on making the api easier to use in other projects

--- webpack.config.js
+++ webpack.config.js
@@ -13,12 +13,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['babel-preset-env']
-          }
-        }
+        loader: 'babel-loader'
       },
       {
         test: /\.scss$/,
