6ddc1ef1dfb64497bd53ee4e51fbe4a171502f79
Adam Berkowitz <berkowitz.clarinet@gmail.com>
2018-07-17 08:12:13 -0400

    fixed arguments to start_lvl

--- src/classes/class-a11y-menu-walker.php
+++ src/classes/class-a11y-menu-walker.php
@@ -22,7 +22,7 @@ class Menu_Walker extends \Walker_Nav_Menu {
   public function end_lvl(&$output, $depth = 0, $args = array()) {
     $output .= "</ul>";
   }
-  public function start_el(&$output, $item, $depth = 0, $args = array()) {
+  public function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) {
     $classes = null;
     
     // this function handles each item in the list
