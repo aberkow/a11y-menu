23f36b1f318fde720647e2a5c742121b8297845e
Adam Berkowitz <berkowitz.clarinet@gmail.com>
2018-09-26 11:39:40 -0400

    working on moving some functionality to the a11y-menu-plugin project

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,10 @@
 import { displayMenu } from './utils/displayMenu';
-
 const testData = require('../mock-data/test-data.json');
 const mainMenu = document.getElementById('main-menu');
-
-const navigation = new Navigation({ click: true });
 
 displayMenu(mainMenu, testData.menu);
 
 document.addEventListener('DOMContentLoaded', () => {
+    const navigation = new Navigation({ click: true });
     navigation.init();
 });
