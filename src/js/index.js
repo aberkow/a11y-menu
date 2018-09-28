d1b5e628547001f3b5be5789cb6a0c9ff6eccd2f
Adam Berkowitz <berkowitz.clarinet@gmail.com>
2018-09-28 10:50:08 -0400

    cleanup

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,6 @@ import Navigation from './Navigation/Navigation';
 const testData = require('../mock-data/test-data.json');
 const mainMenu = document.getElementById('main-menu');
 
-mainMenu.classList.remove('click-menu');
-
 displayMenu(mainMenu, testData.menu);
 const navigation = new Navigation({ click: true });
 
