a952821f183cb85a2d1d9fe69537ee47b5ab4bc2
Adam Berkowitz <berkowitz.clarinet@gmail.com>
2018-11-08 12:21:50 -0500

    submenus automatically close when users tab out

--- src/js/Navigation/Navigation.js
+++ src/js/Navigation/Navigation.js
@@ -15,31 +15,7 @@ class Navigation {
         this.menuId = menuId;
         this.click = click;
     }
-    focusInHandlerOld(evt) {
-        const { target } = evt;
-        this.toggleMenu(target);
-        evt.preventDefault();
-    }
-    keyDownHandlerOld(evt) {
-        const { keyCode } = evt;
-        const expandedElementCollection = document.querySelectorAll('[aria-expanded="true"]')[0];
-        const openSubmenu = document.getElementsByClassName('submenu-list-open')[0];
 
-        if (keyCode === 27 && openSubmenu) {
-            expandedElementCollection.setAttribute('aria-expanded', 'false');
-            openSubmenu.classList.remove('submenu-list-open');
-        }
-    }
-    /**
-     *  When people click on a button, prevent it from gaining focus.
-     *  If the button has focus, it will not allow the submenu to close correctly.
-     *
-     * @param {object} evt
-     * @memberof Navigation
-     */
-    mouseDownHandler(evt) {
-        evt.preventDefault();
-    }
     hoverHandler(evt) {
         const { type, target } = evt;
         if (type === 'mouseout' && target.getAttribute('aria-haspopup') === "true") {
@@ -80,6 +56,7 @@ class Navigation {
         } else {
             // we're near a submenu by clicking on a button but the menu isn't initially open.
             if (target.nextSibling !== null) {
+                console.log(target)
                 submenuList = target.nextSibling;
 
                 // check if there's a nested submenu
@@ -93,39 +70,91 @@ class Navigation {
         }
     }
 
-    escapeHandler(evt) {
-        const { keyCode } = evt;
-        const submenuNodeList = document.querySelectorAll('.submenu-list-open');
-        // const buttonNodeList = document.querySelectorAll('[aria-expanded="true"]')
-        submenuNodeList.forEach((el) => {
-            // toggle all the menus in the NodeList
-            this.toggleSubmenuMenuClass(el);
-        })
-        // buttonNodeList.forEach((el) => {
-        //     this.toggleButtonAria(el);
-        // })
-    }
-
+    /**
+     * 
+     * Handle automatically closing the sub-menus.
+     * When a person opens a sub-menu and then leaves by tabbing, close the sub-menu.
+     * 
+     * @param {object} evt
+     * @return - void
+     * @memberof Navigation
+     */
     focusInHandler(evt) {
-        console.log(evt.target, 'focus!')
+        
+        const { target, target: { offsetParent: { parentNode } } } = evt;
+
+        let expandedButtonNode = this.menu.querySelectorAll('[aria-expanded="true"]');
+        let openElementCollection = this.menu.getElementsByClassName('submenu-list-open');
+
+        // if we leave the menu, clear everything
+        if (!this.menu.contains(target) && expandedButtonNode.length) {
+            this.clearAll();
+        } else {
+            // still in the menu, but moving from one <li> to another
+            // toggle just the button and submenu for the elements that received focusout.
+            expandedButtonNode = parentNode.querySelectorAll('[aria-expanded="true"]');
+            openElementCollection = parentNode.getElementsByClassName('submenu-list-open');
+            
+            // check to make sure that the user hasn't moved to a different menu.
+            if (parentNode.id === this.menuId) {
+                this.toggleButtonAria(expandedButtonNode[0]);
+                this.toggleSubmenuMenuClass(openElementCollection[0]);
+            }
+        }
+        return;
     }
 
+    /**
+     * 
+     * Toggle the class of the submenu element or reset the classes for all menus
+     *
+     * @param {object} el - a submenu (<ul>) element.
+     * @memberof Navigation
+     */
     toggleSubmenuMenuClass(el) {
-        if (el !== null) {
+        if (el !== null && el !== undefined) {
             el.classList.toggle('submenu-list-open');
-            console.log(el)
         } else {
             this.clearMenus();
         }
     }
 
+    /**
+     * Close all submenus
+     * 
+     * @returns void
+     * @memberof Navigation
+     */
     clearMenus() {
         const menuNode = document.querySelectorAll('.submenu-list-open');
         menuNode.forEach(menu => {
             menu.classList.toggle('submenu-list-open');
         })
+        return;
     }
 
+    /**
+     * Toggle all visual icons and set aria-expanded to false.
+     *
+     * @returns void
+     * @memberof Navigation
+     */
+    clearButtons() {
+        const buttonNode = document.querySelectorAll('.submenu-toggle');
+        buttonNode.forEach((button) => {
+            button.setAttribute('aria-expanded', 'false');
+        })
+        return;
+    }
+
+    /**
+     * 
+     * Toggle the state of each button to reflect the aria-expanded state
+     *
+     * @param {*} target - the DOM element returned by evt.target
+     * @returns void
+     * @memberof Navigation
+     */
     toggleButtonAria(target) {
         const buttonNode = document.querySelectorAll('.submenu-toggle');
         
@@ -156,31 +185,52 @@ class Navigation {
                 button.setAttribute('aria-expanded', 'false')
             }
         });
+        return;
     }
 
+    /**
+     * 
+     * Completely reset the state of the menu
+     * 
+     * @returns void
+     * @memberof Navigation
+     */
     clearAll() {
         this.clearMenus();
         this.clearButtons();
+        return;
     }
 
+    /**
+     *
+     * dispatch events to the correct functions.
+     * types include: click, focusin, keydown, mousedown
+     *
+     * @param {object} evt
+     * @returns void
+     * @memberof Navigation
+     */
     eventDispatcher(evt) {
-        // dispatch event listeners to the correct functions.
-
+        
         // mousedown focusin click
         // keydown focusin keydown click
 
         switch (evt.type) {
+            case 'focusin':
+                this.focusInHandler(evt);
+                break;
             case 'keydown':
-                if (evt.keyCode === 9) {
-                    // if the keydown is caused by the tab key, it should be a focusIn
-                    this.focusInHandler(evt);
-                } else if (evt.keyCode === 13) {
+                // if (evt.keyCode === 9) {
+                //     // if the keydown is caused by the tab key, it should be a focusIn
+                //     this.focusInHandler(evt);
+                // } else 
+                if (evt.keyCode === 13) {
                     // if the keydown is caused by the return key, it should be a click
                     // evt.preventDefault();
                     this.clickHandler(evt);
                 } else if (evt.keyCode === 27) {
                     // if the keydown is caused by the escape key, close the menus
-                    this.escapeHandler(evt);
+                    this.clearAll();
                 } else {
                     // throw away all other events.
                     return;
