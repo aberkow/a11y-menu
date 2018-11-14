f75c43963eb37163e63d50f4977cd06e6b4b94e6
Adam Berkowitz <berkowitz.clarinet@gmail.com>
2018-11-14 13:44:01 -0500

    using Array.from and babel/polyfill

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import { displayMenu } from './utils/displayMenu';
 import Navigation from './Navigation/Navigation';
 const testData = require('../mock-data/test-data.json');
-const mainMenu = document.getElementById('main-menu');
+const mainMenu = document.getElementById('am-main-menu');
 
 displayMenu(mainMenu, testData.menu);
 const navigation = new Navigation({ click: true });
