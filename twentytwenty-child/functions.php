d4e993648d37beaeb5d1a2e961eb156128fb25c9
Adam Berkowitz <berkowitz.clarinet@gmail.com>
2020-11-19 12:45:50 -0500

    added very minimal child theme for improved local development

---
+++ twentytwenty-child/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+require get_stylesheet_directory() . '/a11y-menu-src/A11y/Menu_Walker.php';
+
+add_action('wp_enqueue_scripts', 'twentytwenty_child_enqueue_styles');
+add_action('wp_enqueue_scripts', 'twentytwenty_child_enqueue_scripts');
+
+function twentytwenty_child_enqueue_scripts() {
+  wp_register_script('a11y-menu', get_stylesheet_directory_uri() . '/a11y-menu-dist/Navigation.js', [], false, true);
+  wp_enqueue_script('twentytwenty-child', get_stylesheet_directory_uri() . '/index.js', ['a11y-menu'], false, true);
+}
+
+function twentytwenty_child_enqueue_styles() {
+  $parent_handle = 'twentytwenty-style';
+  wp_enqueue_style('a11y-menu', get_stylesheet_directory_uri() . '/a11y-menu-dist/main.css');
+  wp_enqueue_style($parent_handle, get_template_directory_uri() . '/style.css', []);
+  wp_enqueue_style('twentytwenty-child', get_stylesheet_uri(), [$parent_handle, 'a11y-menu']);
+}
